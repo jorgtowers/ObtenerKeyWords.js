68b7136112b56061c1bb00534a3772e63c643016
Jorge Torres <jorgtowers@gmail.com>
2012-10-07 14:34:56 -0430

    ObtenerKeyWords

---
+++ ObtenerKeyWords.js
@@ -0,0 +1,135 @@
+function ObtenerKeyWords(idCampoTexto,idCampoResultado){
+	//Variable Global
+	var _InicioPalabra = "a";
+	var _KeyWordsAObtener=5;
+	var _OmitirSiguientePalabras=[	"a","b","c","d","e","f","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+									"esta","este","cual","como","para","pero","que","por",
+									"uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","diez",
+									"lunes","martes","miercoles","jueves","viernes","sabado","domingo",
+									"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre",									
+									"yo","tu","el","nosotros","vosotros","ellos","ellas",
+									"la","le","li","lo","lu","si","no",
+									",", ";", "?", ".", "$", " ","#"];
+	var _CambiarCaracteresOriginales="√¿¡ƒ¬»…À ÃÕœŒ“”÷‘Ÿ⁄‹€„‡·‰‚ËÈÎÍÏÌÔÓÚÛˆÙ˘˙¸˚—Ò«Á";
+	var _CambiarCaracteresReemplazo="AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc";
+	//Metodos
+	this.KeyWordsAObtener=function(cantidad){
+		if(cantidad.toString()!="")
+			_KeyWordsAObtener=cantidad;
+	}
+	this.OmitirSiguientePalabras=function(palabrasAOmitir)	{		
+		if(palabrasAOmitir.toString()!="")
+			_OmitirSiguientePalabras=palabrasAOmitir;
+	}	
+	this.Sustituir={
+		EstosCaracteres:function(caracterOriginal){
+			if(caracterOriginal.toString()!="")				
+				_CambiarCaracteresOriginales=caracterOriginal;
+		},
+		PorEstosCaracteres:function(caracterASustituir){
+				if(caracterASustituir.toString()!="")					
+				_CambiarCaracteresReemplazo=caracterASustituir;
+		},
+		Aplicar:function(cadena){
+			if(cadena.toString()!="")
+			{
+				var mapping = {}; 
+				for(var i = 0, j = _CambiarCaracteresOriginales.length; i < j; i++ )
+					mapping[ _CambiarCaracteresOriginales.charAt( i ) ] = _CambiarCaracteresReemplazo.charAt( i );
+				var ret = [];
+				for( var i = 0, j = cadena.length; i < j; i++ ) {
+					var c = cadena.charAt( i );
+					if( mapping.hasOwnProperty( cadena.charAt( i ) ) )
+						ret.push( mapping[ c ] );
+					else
+						ret.push( c );
+				}
+				return ret.join( '' );				
+			}
+		}
+		
+	}
+	this.RemoverPalabrasOmitidas=function (palabraAOmitir) {  
+		if(palabraAOmitir.toString()!="")
+		{
+			var flag = 0;  
+			for (var i = 0; i < _OmitirSiguientePalabras.length; i++) {  
+				if (_OmitirSiguientePalabras[i].toLowerCase() == palabraAOmitir.toLowerCase()) {  
+					flag = 1;  
+				}  
+				if (palabraAOmitir == " " || palabraAOmitir == "") {  
+					flag = 1;  
+				}  
+			}  
+			if (flag == 0) {  
+				_InicioPalabra = _InicioPalabra + " " + palabraAOmitir;  
+			}  
+		}
+	}  
+	
+	//Propiedades
+	Texto=document.getElementById(idCampoTexto);
+	Salida=document.getElementById(idCampoResultado);
+	this.KeyWords="";
+	
+	
+	//Proceso de KeyWords
+	this.Procesar=function(){
+		_InicioPalabra="a";
+		KeyWordsEncontrados = new Array(_KeyWordsAObtener);
+		KeyWordsEncontradosFuerza = new Array(_KeyWordsAObtener);		
+		for(a=0;a<_KeyWordsAObtener;a++)
+		{
+			KeyWordsEncontrados[a]="";
+			KeyWordsEncontradosFuerza[a]=0;
+		}
+		var txt = this.Sustituir.Aplicar(Texto.value.replace(/[0-9]/g,'')).replace(/[^a-zA-Z]/g,' ').split(' ');	
+		var palabras = new Array();  
+		totalPalabras=txt.length;  	
+		for(i=0; i<totalPalabras; i++) 
+			this.RemoverPalabrasOmitidas(txt[i]);  
+		txt = _InicioPalabra.replace(/[^a-zA-Z0-9]/g,' ').split(' ');  
+		totalPalabras=txt.length;    
+		for (i=0; i<totalPalabras; i++) {  
+			if (!txt[i])  
+				continue;  
+			if (palabras[txt[i]] == null)  
+				palabras[txt[i]]=0;  
+			palabras[txt[i]]++;  
+		}  
+		salida = new Array();        
+		var palabrasPorIntervalo=Math.round(totalPalabras/_KeyWordsAObtener)
+		for (palabra in palabras) {  
+			salida[salida.length]= palabra +":" +palabras[palabra] + " Fuerza: " + Math.round((palabras[palabra]/totalPalabras)*100) +"%";    			
+			var w = Math.round((palabras[palabra]/totalPalabras)*100);  
+			if(!isNaN(w)) {  
+				var isMayor=false;
+				i=0;
+				while (i<_KeyWordsAObtener)
+				{
+					for(a=i;a<_KeyWordsAObtener;a++)
+					{			
+						if(w>KeyWordsEncontradosFuerza[a])						
+							isMayor=true;						
+						else
+						{
+							isMayor=false;
+							break;				
+						}
+					}				
+					if(isMayor)
+					{
+						KeyWordsEncontradosFuerza[i]=w;
+						KeyWordsEncontrados[i]=palabra;
+						break;
+					}					
+					i++;
+				}		
+			
+			}    
+	   }  
+	  
+		this.KeyWords=KeyWordsEncontrados.toString().replace(/,/g,', ');
+		Salida.value=KeyWordsEncontrados.toString().replace(/,/g,', ');
+	}
+}
---
+++ ReadMe.md
@@ -0,0 +1,43 @@
+
+##ObtenerKeyWords
+Obtiene KeyWords de un texto
+
+	FechaCreacion......:07/10/2012 01:59:37 p.m.
+	Autor..............:Jorge L. Torres A.
+	ArchivoFisico......:ObtenerKeyWords.js
+
+###Metodo: OmitirSiguientePalabras (String[] palabrasaomitir)
+Arreglo de String[] que almacena palabras a ser omitidas en el proceso de obtener KeyWords
+
+*Parametro*: **String[] palabrasaomitir**
+
+	Arreglo de palabras a ser omitidas
+
+
+###Metodo: Sustituir.EstosCaracteres (String caracteroriginal)
+Retorna cadena de caracteres que seran cambiados en el proceso de obtener KeyWords
+
+*Parametro*: **String caracteroriginal**
+
+	Cadena de caracteres especiales a ser cambiados
+
+
+###Metodo: Sustituir.PorEstosCaracteres (String caracterasustituir)
+Retorna cadena de caracteres que cambiaran a los establecidos en la propiedad "Sustituir.EstosCaracteres" en el proceso de obtener KeyWords
+
+*Parametro*: **String caracterasustituir**
+
+	Cadena de caracteres especiales que cambiaran a los establecios en la propidad "Sustituir.EstosCaracteres"
+
+
+###Metodo: KeyWordsAObtener (Int cantidad)
+Retorna Cantidad de KeyWords a retornar
+
+*Parametro*: **Int cantidad**
+
+	Cantidad de KeyWords a recuperar
+
+
+
+
+.
